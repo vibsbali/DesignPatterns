4a2e94a1d7880b6ccfbcd8c1ce836438f66d2c45
Vaibhav Bali <vibsbali@gmail.com>
2016-09-25 18:32:48 +1000

    Added comments

--- DesignPatterns.Strategy/Checkout.cs
+++ DesignPatterns.Strategy/Checkout.cs
@@ -9,6 +9,8 @@
             this.order = order;
         }
 
+        //In theory Chekout class should take an instance of IShippingStrategy instead of Order
+        //And our CalculateShipping method should take Order as a parameter
         //This is all that is left after strategy. No more if else block
         public decimal CalculateShipping(IShippingStrategy shippingMethod)
         {
