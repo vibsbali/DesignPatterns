13489c1a3123894bd26566fc1f340b9156122da5
Vaibhav Bali <vibsbali@gmail.com>
2016-09-25 16:49:48 +1000

    Added a Decorator

---
+++ DesignPatterns.Decorator/PizzaDecorator.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public class PizzaDecorator : AbstractPizza
+    {
+        public PizzaDecorator(AbstractPizza pizza)
+        {
+            Pizza = pizza;
+        }
+
+        public AbstractPizza Pizza { get; private set; }
+
+        public override string GetDescription()
+        {
+            return Pizza.GetDescription();
+        }
+
+        public override decimal CalculatePrice()
+        {
+            return Pizza.CalculatePrice();
+        }
+    }
+}
